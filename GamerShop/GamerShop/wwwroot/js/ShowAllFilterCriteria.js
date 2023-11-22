3cec57eb8f91f59644b5da353278e30a99447fe7
Vlad <snytko.vlad@mail.ru>
2023-11-23 00:11:27 +0100

    Implemented dynamic pagination on the page.

--- GamerShop/GamerShop/wwwroot/js/ShowAllFilterCriteria.js
+++ GamerShop/GamerShop/wwwroot/js/ShowAllFilterCriteria.js
@@ -1,10 +1,9 @@
-﻿$(document).ready(function () {
-
-    function updateSortedData(sortingCriteria, isAscending) {
+﻿$(function () {
+    function updateSortedData(sortingCriteria, isAscending, page, perPage) {
         $.ajax({
             url: '/MovieCollection/UpdateShowAll',
             type: 'GET',
-            data: { sortingCriteria: sortingCriteria, isAscending: isAscending },
+            data: { sortingCriteria: sortingCriteria, isAscending: isAscending, page: page, perPage: perPage },
             success: function (data) {
                 $('#showAllContainer').html(data);
             },
@@ -17,8 +16,23 @@
     $('#sorterCriteria, #sorterDirection').change(function () {
         const selectedSortingCriteria = $('#sorterCriteria').val();
         const selectedIsAscending = $('#sorterDirection').val();
-        updateSortedData(selectedSortingCriteria, selectedIsAscending);
+
+        const page = $('#Page').val();
+        const perPage = $('#PerPage').val();
+
+        updateSortedData(selectedSortingCriteria, selectedIsAscending, 1, perPage);
     });
 
+    $(document).on('click', '.page-link', function (e) {
+        const selectedSortingCriteria = $('#sorterCriteria').val();
+        const selectedIsAscending = $('#sorterDirection').val();
 
+        const page = $(this).data('page');
+        const perPage = $('#PerPage').val();
+
+        e.preventDefault();
+        updateSortedData(selectedSortingCriteria, selectedIsAscending, page, perPage);
+
+        $('html, body').animate({ scrollTop: 0 }, 'fast');
+    });
 });
