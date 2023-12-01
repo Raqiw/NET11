8c370fb62cc3d58bb5672452a6d554fd3eb2de4f
Vlad <snytko.vlad@mail.ru>
2023-12-01 20:52:33 +0100

    Added migration.

---
+++ GamerShop/DALEfDB/Migrations/20231128165514_Added_ImagePath_to_Collection.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DALEfDB.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added_ImagePath_to_Collection : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePath",
+                table: "Collections",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagePath",
+                table: "Collections");
+        }
+    }
+}
